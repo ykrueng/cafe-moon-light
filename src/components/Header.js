49efda872cf93bc52d5342659f4612bb005c1dcc
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-11-04 08:10:17 -0600

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="container">
+        <h1>
+          Cafe <br />
+          Moon Light
+        </h1>
+        <div className="bg-wrapper" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
